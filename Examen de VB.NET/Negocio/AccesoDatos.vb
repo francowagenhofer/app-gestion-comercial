d41d3a3b99882fbc78805efd1a02fafe7089fc1c
franwagenhofer <francowagen@hotmail.com>
2024-11-29 00:39:29 -0300

    Modificaciones en la cadena de conexion a la base de datos.

--- Examen de VB.NET/Negocio/AccesoDatos.vb
+++ Examen de VB.NET/Negocio/AccesoDatos.vb
@@ -1,5 +1,5 @@
 ﻿Imports System.Data.SqlClient
-
+Imports System.Configuration
 
 Namespace Negocio
     Public Class AccesoADatos
@@ -7,29 +7,24 @@ Namespace Negocio
         Private comando As SqlCommand
         Private lector As SqlDataReader
 
-        ' Propiedad para obtener el lector
         Public ReadOnly Property LectorReader As SqlDataReader
             Get
                 Return lector
             End Get
         End Property
 
-        ' Constructor que inicializa la conexión y el comando
         Public Sub New()
+            Dim connectionString As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
+            conexion = New SqlConnection(connectionString)
             'conexion = New SqlConnection("Server=localhost;Uid=sa;Pwd=sasa;MultipleActiveResultSets=True;Timeout=120;Database=pruebademo;")
-
-            conexion = New SqlConnection("Server=WX1\SQLEXPRESS;Database=pruebademo;Integrated Security=True;MultipleActiveResultSets=True;Timeout=120;")
-
             comando = New SqlCommand()
         End Sub
 
-        ' Método para setear las consultas SQL
         Public Sub SetearConsulta(consulta As String)
             comando.CommandType = CommandType.Text
             comando.CommandText = consulta
         End Sub
 
-        ' Método para ejecutar una consulta de lectura (Select)
         Public Sub EjecutarLectura()
             comando.Connection = conexion
             Try
@@ -40,7 +35,6 @@ Namespace Negocio
             End Try
         End Sub
 
-        ' Método para ejecutar acciones (Insert, Update, Delete)
         Public Sub EjecutarAccion()
             comando.Connection = conexion
             Try
@@ -51,12 +45,10 @@ Namespace Negocio
             End Try
         End Sub
 
-        ' Método para agregar parámetros a la consulta
         Public Sub SetearParametro(nombre As String, valor As Object)
             comando.Parameters.AddWithValue(nombre, valor)
         End Sub
 
-        ' Método para cerrar la conexión
         Public Sub CerrarConexion()
             If lector IsNot Nothing Then
                 lector.Close()
